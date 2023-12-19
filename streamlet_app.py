9e844998aeb2b210ff0d13fee3b260f322031000
Muhammad Umair <muhammadumair894@gmail.com>
2023-12-19 20:55:23 +0500

    Create streamlet_app.py

---
+++ streamlet_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Question Answering')
+st.write('Enter a question and a context.')
+question = st.text_input('Question')
+context = st.text_input('Context')
+if st.button('Submit'):
+    model_name = "deepset/roberta-base-squad2"
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    QA_input = {
+        'question': question,
+        'context': context
+    }
+    res = nlp(QA_input)
+    st.write(res['answer'])
